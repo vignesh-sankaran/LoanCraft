e42f2861811f1eb3295743a2ab7f8ad111852d07
Vignesh Sankaran <developer@sankaran.io>
2023-04-07 19:15:07 +1000

    Initial commit

---
+++ HomeCalc/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HomeCalc
+//
+//  Created by Vignesh Sankaran on 7/4/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HomeCalc/HomeCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeCalcApp.swift
+//  HomeCalc
+//
+//  Created by Vignesh Sankaran on 7/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct HomeCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
