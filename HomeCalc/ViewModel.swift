c27b18f335db1586b2a9ce806994ee3150ffb1c6
Vignesh Sankaran <developer@sankaran.io>
2023-05-17 22:58:55 +1000

    Monthly calculator now works :D

--- HomeCalc/ViewModel.swift
+++ HomeCalc/ViewModel.swift
@@ -12,19 +12,23 @@ import SwiftUI
 var cancellable: [AnyCancellable] = []
 
 class ViewModel: ObservableObject {
-    @Published var mortgage = 0
+    @Published var mortgage = 0.0
     @Published var interest = 0.0
     @Published var yearsRemaining = 30
-    @Published var repaymentFrequency = 26
+    @Published var repaymentFrequency = 12
     @Published var mortgageRepayment = 0.0
     
     func calculateMortgageRepayment() {
-        mortgageRepayment = Double(mortgage + 100)
-//        let interestRate = interest / Double(100) / Double(repaymentFrequency)
-//
-//        let topLine = Double(mortgage) * interestRate * pow((1 + interestRate), Double(repaymentFrequency * yearsRemaining))
-//        let bottomLine = pow((1 + interestRate), Double(repaymentFrequency * yearsRemaining)) - 1
-//        mortgageRepayment = topLine / bottomLine
+        let interstForPeriod = interest / Double(repaymentFrequency)
+        let numberOfPeriods = repaymentFrequency * yearsRemaining
+
+        let firstBracket = 1 + interstForPeriod
+        let firstPower = pow(firstBracket, Double(numberOfPeriods))
+        let topLine = mortgage * interstForPeriod * firstPower
+
+        let bottomLine = firstPower - 1
+
+        mortgageRepayment = topLine / bottomLine
     }
 
     init() {
