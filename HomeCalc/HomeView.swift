dbc1e29598d2c40ef5cf5ac3b82299c7d4b288bd
Vignesh Sankaran <developer@sankaran.io>
2024-10-26 22:11:48 +1100

    Initial moving of input fields into their own rows

--- HomeCalc/HomeView.swift
+++ HomeCalc/HomeView.swift
@@ -11,41 +11,31 @@ import SwiftUI
 
 struct HomeView: View {
     @State var viewModel = ViewModel()
-
+    
     var body: some View {
         NavigationView {
             VStack(alignment: .leading) {
-                HStack {
-                    Text("Mortgage amount")
-                    TextField(
-                        "Mortgage amount", value: $viewModel.mortgage,
-                        format: .currency(code: "USD")
-                    )
+                Text("Mortgage amount")
+                TextField(
+                    "Mortgage amount", value: $viewModel.mortgage,
+                    format: .currency(code: "USD")
+                )
+                .keyboardType(.numberPad)
+                Text("Interest")
+                TextField("Interest", value: $viewModel.interest, format: .percent)
+                    .keyboardType(
+                        .decimalPad)
+                Text("Years remaining")
+                TextField("Years remaining", value: $viewModel.yearsRemaining, format: .number)
                     .keyboardType(.numberPad)
-                }
-                HStack {
-                    Text("Interest")
-                    TextField("Interest", value: $viewModel.interest, format: .percent)
-                        .keyboardType(
-                            .decimalPad)
-                }
-                HStack {
-                    Text("Years remaining")
-                    TextField("Years remaining", value: $viewModel.yearsRemaining, format: .number)
-                        .keyboardType(.numberPad)
-                }
                 // Set up saving of value if on focus, then off focus has a 0, empty, or invalid value
-                HStack {
-                    Text("Repayment frequency")
-                    Picker("Repayment frequency", selection: $viewModel.repaymentFrequency) {
-                        Text("Monthly").tag(12)
-                        Text("Fortnightly").tag(26)
-                    }.pickerStyle(.segmented)
-                }
-                HStack {
-                    Text("Repayment amount")
-                    Text(viewModel.mortgageRepayment, format: .currency(code: "USD"))
-                }
+                Text("Repayment frequency")
+                Picker("Repayment frequency", selection: $viewModel.repaymentFrequency) {
+                    Text("Monthly").tag(12)
+                    Text("Fortnightly").tag(26)
+                }.pickerStyle(.segmented)
+                Text("Repayment amount")
+                Text(viewModel.mortgageRepayment, format: .currency(code: "USD"))
                 Chart {
                     BarMark(
                         x: .value("", ""), y: .value("Total amount", viewModel.chartData.principal), width: .ratio(0.85)
