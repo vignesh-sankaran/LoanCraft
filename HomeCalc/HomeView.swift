dc6200af74751c22e2976201ad445893a6665d85
Vignesh Sankaran <developer@sankaran.io>
2024-11-13 20:49:11 +1100

    Make view scrollable

--- HomeCalc/HomeView.swift
+++ HomeCalc/HomeView.swift
@@ -13,60 +13,62 @@ struct HomeView: View {
     @State var viewModel = ViewModel()
     var body: some View {
         NavigationStack {
-            VStack(alignment: .leading) {
-                Text("Mortgage amount")
-                TextField(
-                    "Mortgage amount", value: $viewModel.mortgage,
-                    format: .currency(code: "AUD")
-                )
-                .keyboardType(.numberPad)
-                .padding(.bottom, 16)
-                Text("Interest")
-                TextField("Interest", value: $viewModel.interest, format: .percent)
-                    .keyboardType(
-                        .decimalPad)
-                    .padding(.bottom, 16)
-                Text("Years remaining")
-                TextField("Years remaining", value: $viewModel.yearsRemaining, format: .number)
+            ScrollView {
+                VStack(alignment: .leading) {
+                    Text("Mortgage amount")
+                    TextField(
+                        "Mortgage amount", value: $viewModel.mortgage,
+                        format: .currency(code: "AUD")
+                    )
                     .keyboardType(.numberPad)
-                    .toolbar {
-                        ToolbarItemGroup(placement: .keyboard) {
-                            Button("Left", systemImage: "chevron.left") {}
-                            Button("Right", systemImage: "chevron.right") {}
-                            Spacer()
-                            Button("Done") {
-                                hideKeyboard()
+                    .padding(.bottom, 16)
+                    Text("Interest")
+                    TextField("Interest", value: $viewModel.interest, format: .percent)
+                        .keyboardType(
+                            .decimalPad)
+                        .padding(.bottom, 16)
+                    Text("Years remaining")
+                    TextField("Years remaining", value: $viewModel.yearsRemaining, format: .number)
+                        .keyboardType(.numberPad)
+                        .toolbar {
+                            ToolbarItemGroup(placement: .keyboard) {
+                                Button("Left", systemImage: "chevron.left") {}
+                                Button("Right", systemImage: "chevron.right") {}
+                                Spacer()
+                                Button("Done") {
+                                    hideKeyboard()
+                                }
                             }
                         }
+                        .padding(.bottom, 16)
+                    // Set up saving of value if on focus, then off focus has a 0, empty, or invalid value
+                    Text("Repayment frequency")
+                    Picker("Repayment frequency", selection: $viewModel.repaymentFrequency) {
+                        Text("Weekly").tag(52)
+                        Text("Fortnightly").tag(26)
+                        Text("Monthly").tag(12)
+                        Text("Yearly").tag(1)
                     }
+                    .pickerStyle(.segmented)
+                    .sensoryFeedback(.selection, trigger: viewModel.repaymentFrequency)
                     .padding(.bottom, 16)
-                // Set up saving of value if on focus, then off focus has a 0, empty, or invalid value
-                Text("Repayment frequency")
-                Picker("Repayment frequency", selection: $viewModel.repaymentFrequency) {
-                    Text("Weekly").tag(52)
-                    Text("Fortnightly").tag(26)
-                    Text("Monthly").tag(12)
-                    Text("Yearly").tag(1)
+                    Text("Repayment amount per period:").bold()
+                    Text(viewModel.mortgageRepayment, format: .currency(code: "AUD")).padding(.bottom, 32)
+                    Chart {
+                        BarMark(
+                            x: .value("", ""), y: .value("Total amount", viewModel.chartData.principal), width: .ratio(0.85)
+                        )
+                        .foregroundStyle(by: .value("Principal", "Principal"))
+                        BarMark(x: .value("", ""), y: .value("Interest", viewModel.chartData.interest), width: .ratio(0.85))
+                            .foregroundStyle(by: .value("Interest", "Interest"))
+                            .annotation {
+                                Text(viewModel.chartData.formattedTotal ?? "").font(.title3).bold()
+                            }
+                    }
                 }
-                .pickerStyle(.segmented)
-                .sensoryFeedback(.selection, trigger: viewModel.repaymentFrequency)
-                .padding(.bottom, 16)
-                Text("Repayment amount per period:").bold()
-                Text(viewModel.mortgageRepayment, format: .currency(code: "AUD")).padding(.bottom, 32)
-                Chart {
-                    BarMark(
-                        x: .value("", ""), y: .value("Total amount", viewModel.chartData.principal), width: .ratio(0.85)
-                    )
-                    .foregroundStyle(by: .value("Principal", "Principal"))
-                    BarMark(x: .value("", ""), y: .value("Interest", viewModel.chartData.interest), width: .ratio(0.85))
-                        .foregroundStyle(by: .value("Interest", "Interest"))
-                        .annotation {
-                            Text(viewModel.chartData.formattedTotal ?? "").font(.title3).bold()
-                        }
-                }
+                .textFieldStyle(.roundedBorder)
+                .padding()
             }
-            .textFieldStyle(.roundedBorder)
-            .padding()
             .navigationTitle("HomeCalc")
             .toolbarBackgroundVisibility(.visible, for: .navigationBar)
         }
