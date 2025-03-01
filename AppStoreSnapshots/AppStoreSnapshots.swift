486eaa3104aea8f81835428969ea7cdc2c8c361a
Vignesh Sankaran <developer@sankaran.io>
2025-03-02 01:08:24 +1100

    Next iteration of screenshot automation

--- AppStoreSnapshots/AppStoreSnapshots.swift
+++ AppStoreSnapshots/AppStoreSnapshots.swift
@@ -8,6 +8,9 @@
 import XCTest
 
 final class AppStoreSnapshots: XCTestCase {
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
     private var app: XCUIApplication!
     
     override func setUp() {
@@ -19,18 +22,31 @@ final class AppStoreSnapshots: XCTestCase {
     
     override func tearDown() {
         app = nil
+        continueAfterFailure = false
         super.tearDown()
     }
 
-    func test_full_screen() {}
+    func test_full_screen() {
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.lifetime = .keepAlways
+
+    }
 
     func test_tap_mortgage_field() {
         let firstTextField = app.textFields.firstMatch
         firstTextField.tap()
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Enter your mortgage amount"
+        attachment.lifetime = .keepAlways
+        add(attachment)
     }
 
     func test_tap_chart() {
         app.swipeUp()
         app.otherElements["interest-bar-mark"].tap()
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Tap the graph to see principal and interest"
+        attachment.lifetime = .keepAlways
+        add(attachment)
     }
 }
