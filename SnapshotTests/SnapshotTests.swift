f9f3a7e05455e87cd3c08ca5d30f19af29927c97
Vignesh Sankaran <developer@sankaran.io>
2025-03-06 22:13:18 +1100

    Add snapshot testing library

---
+++ SnapshotTests/SnapshotTests.swift
@@ -0,0 +1,14 @@
+//
+//  SnapshotTests.swift
+//  SnapshotTests
+//
+//  Created by Vignesh Sankaran on 6/3/2025.
+//
+
+import XCTest
+
+final class SnapshotTests: XCTestCase {
+    func test_default() {
+        
+    }
+}
