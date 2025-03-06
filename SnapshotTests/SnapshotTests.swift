05fb742a6d0f5977903c260e714aa8bd98359a54
Vignesh Sankaran <developer@sankaran.io>
2025-03-06 22:35:03 +1100

    Add a screenshot test =D

--- SnapshotTests/SnapshotTests.swift
+++ SnapshotTests/SnapshotTests.swift
@@ -5,10 +5,15 @@
 //  Created by Vignesh Sankaran on 6/3/2025.
 //
 
+import SnapshotTesting
 import XCTest
+
+@testable import LoanCraft
 
 final class SnapshotTests: XCTestCase {
     func test_default() {
+        let view = LoanCraftView()
         
+        assertSnapshot(of: view, as: .image)
     }
 }
