d4acf7772928c7650e3af8b4366bb00ad038998d
Vignesh Sankaran <developer@sankaran.io>
2025-03-07 19:27:19 +1100

    Update snapshots

--- SnapshotTests/SnapshotTests.swift
+++ SnapshotTests/SnapshotTests.swift
@@ -14,7 +14,12 @@ final class SnapshotTests: XCTestCase {
     func test_default() {
         let view = LoanCraftView()
         
-        assertSnapshot(of: view, as: .image)
-        assertSnapshot(of: view, as: .image(traits: .init(userInterfaceStyle: .dark)))
+        assertSnapshot(of: view, as: .image(layout: .device(config: .iPhone8)))
+        assertSnapshot(of: view, as: .image(layout: .device(config: .iPhoneSe)))
+        assertSnapshot(of: view, as: .image(layout: .device(config: .iPhone13ProMax)))
+        assertSnapshot(of: view, as: .image(layout: .device(config: .iPhone8), traits: .init(userInterfaceStyle: .dark)))
+        assertSnapshot(of: view, as: .image(layout: .device(config: .iPad10_2)))
+        assertSnapshot(of: view, as: .image(layout: .device(config: .iPad10_2(.landscape))))
+        assertSnapshot(of: view, as: .image(layout: .device(config: .iPad10_2), traits: .init(userInterfaceStyle: .dark)))
     }
 }
