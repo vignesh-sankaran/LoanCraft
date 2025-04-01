e88834e413844093c3435ab2fee41f5b1ca0c54c
Vignesh Sankaran <developer@sankaran.io>
2025-04-01 22:05:21 +1100

    Dismiss keyboard when anywhere outside of text field is selected

--- LoanCraft/LoanCraftView.swift
+++ LoanCraft/LoanCraftView.swift
@@ -20,99 +20,108 @@ struct LoanCraftView: View {
         NavigationStack {
             ScrollView {
                 VStack(alignment: .leading) {
-                    Text("Mortgage amount")
-                    TextField(
-                        "Mortgage amount", value: $viewModel.mortgage,
-                        format: .currency(code: Locale.current.currency?.identifier ?? "USD")
-                    )
-                    .textFieldStyle(.roundedBorder)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 8)
-                            .stroke(
-                                selectedTextField == .mortgageAmount ? Color.blue : Color("UnselectedFieldBorder"),
-                                lineWidth: selectedTextField == .mortgageAmount ? 2 : 0.5
-                            )
-                    )
-                    .focused($selectedTextField, equals: .mortgageAmount)
-                    .keyboardType(.numberPad)
-                    .onChange(of: selectedTextField) { _, newValue in
-                        if let newValue {
-                            analytics.track(newValue.trackingValue)
-                        }
-                    }
-                    .padding(.bottom, 16)
-                    Text("Interest")
-                    TextField("Interest", value: $viewModel.interest, format: .percent)
+                    VStack(alignment: .leading) {
+                        Text("Mortgage amount")
+                        TextField(
+                            "Mortgage amount", value: $viewModel.mortgage,
+                            format: .currency(code: Locale.current.currency?.identifier ?? "USD")
+                        )
                         .textFieldStyle(.roundedBorder)
                         .overlay(
                             RoundedRectangle(cornerRadius: 8)
                                 .stroke(
-                                    selectedTextField == .interest ? Color.blue : Color("UnselectedFieldBorder"),
-                                    lineWidth: selectedTextField == .interest ? 2 : 0.5
+                                    selectedTextField == .mortgageAmount ? Color.blue : Color("UnselectedFieldBorder"),
+                                    lineWidth: selectedTextField == .mortgageAmount ? 2 : 0.5
                                 )
                         )
-                        .keyboardType(.decimalPad)
-                        .focused($selectedTextField, equals: .interest)
-                        .padding(.bottom, 16)
-                    Text("Years remaining")
-                    TextField("Years remaining", value: $viewModel.yearsRemaining, format: .number)
-                        .textFieldStyle(.roundedBorder)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 8)
-                                .stroke(
-                                    selectedTextField ==    .yearsRemaining ? Color.blue : Color("UnselectedFieldBorder"),
-                                    lineWidth: selectedTextField == .yearsRemaining ? 2 : 0.5
-                                )
-                        )
+                        .focused($selectedTextField, equals: .mortgageAmount)
                         .keyboardType(.numberPad)
-                        .focused($selectedTextField, equals: .yearsRemaining)
-                        .toolbar {
-                            ToolbarItemGroup(placement: .keyboard) {
-                                Button("Up", systemImage: "chevron.up") {
-                                    analytics.track(.keyboardUpButtonTapped)
-                                    selectedTextField = selectedTextField?.previous
-                                }.fontWeight(.bold).disabled(selectedTextField == .mortgageAmount)
-                                Button("Down", systemImage: "chevron.down") {
-                                    analytics.track(.keyboardDownButtonTapped)
-                                    selectedTextField = selectedTextField?.next
-                                }.fontWeight(.bold).disabled(selectedTextField == .yearsRemaining)
-                                Spacer()
-                                Button("Done") {
-                                    analytics.track(.doneButtonTapped)
-                                    hideKeyboard()
-                                }.bold()
+                        .onChange(of: selectedTextField) { _, newValue in
+                            if let newValue {
+                                analytics.track(newValue.trackingValue)
                             }
                         }
                         .padding(.bottom, 16)
-                    // Set up saving of value if on focus, then off focus has a 0, empty, or invalid value
-                    Text("Repayment frequency")
-                    Picker("Repayment frequency", selection: $viewModel.repaymentFrequency) {
-                        Text("Weekly").tag(RepaymentFrequency.week)
-                        Text("Fortnightly").tag(RepaymentFrequency.fortnight)
-                        Text("Monthly").tag(RepaymentFrequency.month)
-                        Text("Yearly").tag(RepaymentFrequency.year)
-                    }
-                    .pickerStyle(.segmented)
-                    .sensoryFeedback(.selection, trigger: viewModel.repaymentFrequency)
-                    .padding(.bottom, 16)
-                    .onChange(of: viewModel.repaymentFrequency) { _, newValue in
-                        analytics.track(
-                            .repaymentFrequencySlider,
-                            properties: ["selectedFrequency": newValue.rawValue]
-                        )
-                    }
-                    VStack(
-                        alignment: .leading,
-                        spacing: 4
-                    ) {
-                        Text("Repayment amount per \(viewModel.repaymentFrequency.rawValue):").bold()
-                        SelectableTextField(
-                            text: $viewModel.formattedMortgagePayment,
-                            type: .mortgagePayment
-                        )
-                        .offset(x: -5)
+                        Text("Interest")
+                        TextField("Interest", value: $viewModel.interest, format: .percent)
+                            .textFieldStyle(.roundedBorder)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 8)
+                                    .stroke(
+                                        selectedTextField == .interest ? Color.blue : Color("UnselectedFieldBorder"),
+                                        lineWidth: selectedTextField == .interest ? 2 : 0.5
+                                    )
+                            )
+                            .keyboardType(.decimalPad)
+                            .focused($selectedTextField, equals: .interest)
+                            .padding(.bottom, 16)
+                        Text("Years remaining")
+                        TextField("Years remaining", value: $viewModel.yearsRemaining, format: .number)
+                            .textFieldStyle(.roundedBorder)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 8)
+                                    .stroke(
+                                        selectedTextField ==    .yearsRemaining ? Color.blue : Color("UnselectedFieldBorder"),
+                                        lineWidth: selectedTextField == .yearsRemaining ? 2 : 0.5
+                                    )
+                            )
+                            .keyboardType(.numberPad)
+                            .focused($selectedTextField, equals: .yearsRemaining)
+                            .toolbar {
+                                ToolbarItemGroup(placement: .keyboard) {
+                                    Button("Up", systemImage: "chevron.up") {
+                                        analytics.track(.keyboardUpButtonTapped)
+                                        selectedTextField = selectedTextField?.previous
+                                    }.fontWeight(.bold).disabled(selectedTextField == .mortgageAmount)
+                                    Button("Down", systemImage: "chevron.down") {
+                                        analytics.track(.keyboardDownButtonTapped)
+                                        selectedTextField = selectedTextField?.next
+                                    }.fontWeight(.bold).disabled(selectedTextField == .yearsRemaining)
+                                    Spacer()
+                                    Button("Done") {
+                                        analytics.track(.doneButtonTapped)
+                                        hideKeyboard()
+                                    }.bold()
+                                }
+                            }
+                            .padding(.bottom, 16)
+                        // Set up saving of value if on focus, then off focus has a 0, empty, or invalid value
+                        Text("Repayment frequency")
+                        Picker("Repayment frequency", selection: $viewModel.repaymentFrequency) {
+                            Text("Weekly").tag(RepaymentFrequency.week)
+                            Text("Fortnightly").tag(RepaymentFrequency.fortnight)
+                            Text("Monthly").tag(RepaymentFrequency.month)
+                            Text("Yearly").tag(RepaymentFrequency.year)
+                        }
+                        .pickerStyle(.segmented)
+                        .sensoryFeedback(.selection, trigger: viewModel.repaymentFrequency)
                         .padding(.bottom, 16)
+                        .onChange(of: viewModel.repaymentFrequency) { _, newValue in
+                            analytics.track(
+                                .repaymentFrequencySlider,
+                                properties: ["selectedFrequency": newValue.rawValue]
+                            )
+                        }
+                        VStack(
+                            alignment: .leading,
+                            spacing: 4
+                        ) {
+                            Text("Repayment amount per \(viewModel.repaymentFrequency.rawValue):").bold()
+                            SelectableTextField(
+                                text: $viewModel.formattedMortgagePayment,
+                                type: .mortgagePayment
+                            )
+                            .offset(x: -5)
+                            .padding(.bottom, 16)
+                        }
                     }
+                    .background(
+                        Color.clear
+                            .contentShape(Rectangle())
+                            .onTapGesture {
+                                hideKeyboard()
+                            }
+                    )
                     Chart {
                         BarMark(
                             x: .value("", ""),
