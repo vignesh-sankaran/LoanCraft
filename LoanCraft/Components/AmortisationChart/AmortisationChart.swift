1ec513fa535d33ad2131e532f7554adfc1e88dbb
Vignesh Sankaran <developer@sankaran.io>
2025-06-16 21:18:45 +1000

    Remove feature toggle from view
    Will just ship it as is

--- LoanCraft/Components/AmortisationChart/AmortisationChart.swift
+++ LoanCraft/Components/AmortisationChart/AmortisationChart.swift
@@ -9,129 +9,126 @@ import Charts
 import SwiftUI
 
 struct AmortisationChart: View {
-    @State private var featureFlagService = FeatureFlagService.instance
     @State var viewModel = AmortisationViewModel()
     @Environment(LoanCraftViewModel.self) private var loanCraftViewModel
 
     var body: some View {
-        if featureFlagService.amortisationGraphEnabled {
-            Chart {
-                ForEach(viewModel.schedule) { amortisationData in
-                    LineMark(
-                        x: .value("Year", amortisationData.year),
-                        y: .value("Remaining", amortisationData.remaining)
-                    )
-                    RuleMark(x: .value("Year", viewModel.selectedYear))
-                        .foregroundStyle(.red)
-                        .lineStyle(StrokeStyle(lineWidth: 1))
-                }
+        Chart {
+            ForEach(viewModel.schedule) { amortisationData in
+                LineMark(
+                    x: .value("Year", amortisationData.year),
+                    y: .value("Remaining", amortisationData.remaining)
+                )
+                RuleMark(x: .value("Year", viewModel.selectedYear))
+                    .foregroundStyle(.red)
+                    .lineStyle(StrokeStyle(lineWidth: 1))
             }
-            .chartYAxis {
-                AxisMarks { value in
-                    AxisValueLabel {
-                        if let decimalValue = value.as(Decimal.self) {
-                            Text(
-                                decimalValue.currencyFormatted(
-                                    maximumFractionDigits: 0
-                                )
-                            )
-                        }
-                    }
-                }
-            }
-            .chartXAxisLabel(
-                position: .bottom,
-                alignment: .center
-            ) {
-                Text("Years")
-                    .font(.headline)
-            }
-            .onAppear {
-                viewModel.calculateSchedule(with: loanCraftViewModel)
-            }
-            .chartOverlay { chartProxy in
-                GeometryReader { gemoetryProxy in
-                    Rectangle().fill(.clear).contentShape(Rectangle())
-                        .gesture(
-                            SpatialTapGesture()
-                                .onEnded { value in
-                                    viewModel.selectedYear = findElement(
-                                        location: value.location, chartProxy: chartProxy,
-                                        geometryProxy: gemoetryProxy
-                                    )
-                                }
-                                .exclusively(
-                                    before: DragGesture()
-                                        .onChanged { value in
-                                            viewModel.selectedYear = findElement(
-                                                location: value.location, chartProxy: chartProxy,
-                                                geometryProxy: gemoetryProxy
-                                            )
-                                        }
-                                )
-                        )
-                }
-            }
-            .chartOverlay { chartProxy in
-                GeometryReader { geometryProxy in
-                    if let chartProxyPlotFrame = chartProxy.plotFrame {
-                        let startPositionX = chartProxy.position(forX: viewModel.selectedYear) ?? 0
-
-                        let lineX = startPositionX + geometryProxy[chartProxyPlotFrame].origin.x
-                        let overlayWidth: CGFloat = 115
-                        let overlayOffset = max(
-                            0,
-                            min(
-                                chartProxy.plotSize.width - overlayWidth, lineX - (overlayWidth / 2)
-                            )
-                        )
-                        VStack(
-                            alignment: .center,
-                            spacing: 4
-                        ) {
-                            Text("Balance")
-                                .font(.headline)
-                            SelectableTextField(
-                                text: .constant(
-                                    viewModel.principalRemaining
-                                ),
-                                type: .mortgagePayment
-                            )
-                            .multilineTextAlignment(.center)
-                            Text("Years left")
-                                .font(.headline)
-                            SelectableTextField(
-                                text: .constant(
-                                    viewModel.yearsRemaining
-                                ),
-                                type: .mortgagePayment
-                            )
-                            .multilineTextAlignment(.center)
-                        }
-                        .frame(width: overlayWidth)
-                        .padding(.top, 2)
-                        .padding(.bottom, 4)
-                        .background {
-                            ZStack {
-                                RoundedRectangle(cornerRadius: 8)
-                                    .fill(.background.opacity(0.9))
-                                RoundedRectangle(cornerRadius: 8)
-                                    .fill(.quaternary.opacity(0.9))
-                            }
-                            .padding(.horizontal, -8)
-                            .padding(.vertical, -4)
-                        }
-                        .offset(x: overlayOffset)
-                    }
-                }
-            }
-            .frame(height: 350)
-            .sensoryFeedback(
-                .impact(
-                    intensity: 0.4
-                ),
-                trigger: viewModel.selectedYear
-            )
         }
+        .chartYAxis {
+            AxisMarks { value in
+                AxisValueLabel {
+                    if let decimalValue = value.as(Decimal.self) {
+                        Text(
+                            decimalValue.currencyFormatted(
+                                maximumFractionDigits: 0
+                            )
+                        )
+                    }
+                }
+            }
+        }
+        .chartXAxisLabel(
+            position: .bottom,
+            alignment: .center
+        ) {
+            Text("Years")
+                .font(.headline)
+        }
+        .onAppear {
+            viewModel.calculateSchedule(with: loanCraftViewModel)
+        }
+        .chartOverlay { chartProxy in
+            GeometryReader { gemoetryProxy in
+                Rectangle().fill(.clear).contentShape(Rectangle())
+                    .gesture(
+                        SpatialTapGesture()
+                            .onEnded { value in
+                                viewModel.selectedYear = findElement(
+                                    location: value.location, chartProxy: chartProxy,
+                                    geometryProxy: gemoetryProxy
+                                )
+                            }
+                            .exclusively(
+                                before: DragGesture()
+                                    .onChanged { value in
+                                        viewModel.selectedYear = findElement(
+                                            location: value.location, chartProxy: chartProxy,
+                                            geometryProxy: gemoetryProxy
+                                        )
+                                    }
+                            )
+                    )
+            }
+        }
+        .chartOverlay { chartProxy in
+            GeometryReader { geometryProxy in
+                if let chartProxyPlotFrame = chartProxy.plotFrame {
+                    let startPositionX = chartProxy.position(forX: viewModel.selectedYear) ?? 0
+
+                    let lineX = startPositionX + geometryProxy[chartProxyPlotFrame].origin.x
+                    let overlayWidth: CGFloat = 115
+                    let overlayOffset = max(
+                        0,
+                        min(
+                            chartProxy.plotSize.width - overlayWidth, lineX - (overlayWidth / 2)
+                        )
+                    )
+                    VStack(
+                        alignment: .center,
+                        spacing: 4
+                    ) {
+                        Text("Balance")
+                            .font(.headline)
+                        SelectableTextField(
+                            text: .constant(
+                                viewModel.principalRemaining
+                            ),
+                            type: .mortgagePayment
+                        )
+                        .multilineTextAlignment(.center)
+                        Text("Years left")
+                            .font(.headline)
+                        SelectableTextField(
+                            text: .constant(
+                                viewModel.yearsRemaining
+                            ),
+                            type: .mortgagePayment
+                        )
+                        .multilineTextAlignment(.center)
+                    }
+                    .frame(width: overlayWidth)
+                    .padding(.top, 2)
+                    .padding(.bottom, 4)
+                    .background {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 8)
+                                .fill(.background.opacity(0.9))
+                            RoundedRectangle(cornerRadius: 8)
+                                .fill(.quaternary.opacity(0.9))
+                        }
+                        .padding(.horizontal, -8)
+                        .padding(.vertical, -4)
+                    }
+                    .offset(x: overlayOffset)
+                }
+            }
+        }
+        .frame(height: 350)
+        .sensoryFeedback(
+            .impact(
+                intensity: 0.4
+            ),
+            trigger: viewModel.selectedYear
+        )
     }
 }
